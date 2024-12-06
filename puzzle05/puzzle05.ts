6f579e16b16289f7a44b19a5eac059c64ffd2f96
Kamil Drzastwa <drzastwakamil@gmail.com>
2024-12-06 01:25:39 +0100

    [AOC] Puzzle 5 creating correct function to validate order. Test cases met.

--- puzzle05/puzzle05.ts
+++ puzzle05/puzzle05.ts
@@ -1,128 +1,24 @@
-type Character = "X" | "M" | "A" | "S";
-export function countXMAS(input: string): number {
-  const horizontal = input.split("\n").map((line) =>
-    line.split("") as Array<Character>
-  );
-
-  const vertical = Array.from(
-    { length: horizontal[0].length },
-    (): Array<Character> => [],
-  );
-
-  for (const horizontalLine of horizontal) {
-    for (const [index, character] of horizontalLine.entries()) {
-      vertical[index].push(character);
-    }
-  }
-
-  const diagonalLTR: Array<Array<Character>> = [];
-
-  for (let index = 0; index < horizontal[0].length - 3; index++) {
-    const diagonalLine: Array<Character> = [];
+export function checkIfPageNumbersMeetOrderRules(
+  orderRules: Set<string>,
+  pageNumbers: Array<string>,
+) {
+  for (const [index, pageNumber] of pageNumbers.entries()) {
     for (
-      let x = index,
-        y = 0;
-      x < horizontal[0].length && y < vertical.length;
-      x++, y++
+      let nextNumberIndex = index + 1;
+      nextNumberIndex < pageNumbers.length;
+      nextNumberIndex++
     ) {
-      diagonalLine.push(horizontal[y][x]);
-    }
-    diagonalLTR.push(diagonalLine);
-  }
-
-  for (let index = 1; index < vertical[0].length - 3; index++) {
-    const diagonalLine: Array<Character> = [];
-    for (
-      let x = 0,
-        y = index;
-      x < horizontal[0].length && y < vertical.length;
-      x++, y++
-    ) {
-      diagonalLine.push(horizontal[y][x]);
-    }
-    diagonalLTR.push(diagonalLine);
-  }
-
-  const diagonalRTL: Array<Array<Character>> = [];
-  for (let index = horizontal[0].length - 1; index > 2; index--) {
-    const diagonalLine: Array<Character> = [];
-    for (
-      let x = index,
-        y = 0;
-      x >= 0 && y < vertical.length;
-      x--, y++
-    ) {
-      diagonalLine.push(horizontal[y][x]);
-    }
-    diagonalRTL.push(diagonalLine);
-  }
-
-  for (let index = 1; index < vertical[0].length - 3; index++) {
-    const diagonalLine: Array<Character> = [];
-    for (
-      let x = horizontal[0].length - 1,
-        y = index;
-      x >= 0 && y < vertical.length;
-      x--, y++
-    ) {
-      diagonalLine.push(horizontal[y][x]);
-    }
-    diagonalRTL.push(diagonalLine);
-  }
-
-  let result = 0;
-  for (const line of horizontal) {
-    result += countInLineXMAS(line);
-  }
-
-  for (const line of vertical) {
-    result += countInLineXMAS(line);
-  }
-
-  for (const line of diagonalLTR) {
-    result += countInLineXMAS(line);
-  }
-
-  for (const line of diagonalRTL) {
-    result += countInLineXMAS(line);
-  }
-
-  return result;
-}
-
-export function countInLineXMAS(line: Array<Character>) {
-  let count = 0;
-  let looksFor: "XMAS" | "SAMX" | undefined;
-  let index = 0;
-
-  for (const character of line) {
-    if (looksFor) {
-      if (character === looksFor[index]) {
-        index += 1;
-        if (index === 4) {
-          count += 1;
-          looksFor = looksFor === "SAMX" ? "XMAS" : "SAMX";
-          index = 1;
-        }
-      } else {
-        looksFor = undefined;
-      }
-    }
-
-    if (!looksFor) {
-      if (character === "X") {
-        looksFor = "XMAS";
-        index = 1;
-      } else if (character === "S") {
-        looksFor = "SAMX";
-        index = 1;
+      const rule = `${pageNumber}|${pageNumbers[nextNumberIndex]}`;
+      if (!orderRules.has(rule)) {
+        return false;
       }
     }
   }
 
-  return count;
+  return true;
 }
 
-const input = await Deno.readTextFile("./puzzle04/puzzle04_input.txt");
-const result = countXMAS(input);
+const input = await Deno.readTextFile("./puzzle05/puzzle05_input.txt");
+
+const result = 0;
 console.log("the result ", result);
--- puzzle05/puzzle05_test.ts
+++ puzzle05/puzzle05_test.ts
@@ -1,12 +1,117 @@
 import { assertEquals } from "@std/assert";
-import {  } from "./puzzle05.ts";
+import { checkIfPageNumbersMeetOrderRules } from "./puzzle05.ts";
 
-Deno.test("countXMAS - test case 1", () => {
-  const result = 0
-  const expected = 0;
+const rules = new Set([
+  "47|53",
+  "97|13",
+  "97|61",
+  "97|47",
+  "75|29",
+  "61|13",
+  "75|53",
+  "29|13",
+  "97|29",
+  "53|29",
+  "61|53",
+  "97|53",
+  "61|29",
+  "47|13",
+  "75|47",
+  "97|75",
+  "47|61",
+  "75|61",
+  "47|29",
+  "75|13",
+  "53|13",
+]);
+
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 1", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "75",
+    "47",
+    "61",
+    "53",
+    "29",
+  ]);
+  const expected = true;
   assertEquals(
     result,
     expected,
-    `Expected ${18} but got ${result} for input test puzzle input.`,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
+  );
+});
+
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 2", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "97",
+    "61",
+    "53",
+    "29",
+    "13",
+  ]);
+  const expected = true;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
+  );
+});
+
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 3", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "75",
+    "29",
+    "13",
+  ]);
+  const expected = true;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
+  );
+});
+
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 4", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "75",
+    "97",
+    "47",
+    "61",
+    "53",
+  ]);
+  const expected = false;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
+  );
+});
+
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 5", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "61",
+    "13",
+    "29",
+  ]);
+  const expected = false;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
+  );
+});
+Deno.test("checkIfPageNumbersMeetOrderRules - test case 6", () => {
+  const result = checkIfPageNumbersMeetOrderRules(rules, [
+    "97",
+    "13",
+    "75",
+    "29",
+    "47",
+  ]);
+  const expected = false;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result} for input test puzzle input.`,
   );
 });
