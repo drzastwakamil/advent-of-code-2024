12186e0643b14173611c225737edb4177b4de64a
Kamil Drzastwa <drzastwakamil@gmail.com>
2024-12-27 11:17:25 +0100

    [AOC] Puzzle 2 - solving part 2

--- puzzle02/puzzle02.ts
+++ puzzle02/puzzle02.ts
@@ -13,13 +13,13 @@ function checkIfIncreases(a: number, b: number) {
 function checkIfDecreases(a: number, b: number) {
   return a > b;
 }
-export function isReportLevelsSafe(line: string): boolean {
+export function isReportLevelsSafe(line: string): [boolean, number] {
   const [firstLevel, ...levels] = line.split(" ");
 
   const firstLevelValue = Number(firstLevel);
   const secondLevelValue = Number(levels[0]);
   if (differenceDoesNotMeetBaseCriteria(firstLevelValue, secondLevelValue)) {
-    return false;
+    return [false, 0];
   }
 
   const meetsIncreaseOrDecreaseCriteria =
@@ -31,23 +31,56 @@ export function isReportLevelsSafe(line: string): boolean {
     const levelValue = Number(levels[levelIndex]);
 
     if (differenceDoesNotMeetBaseCriteria(previousLevelValue, levelValue)) {
-      return false;
+      return [false, levelIndex];
     }
     if (meetsIncreaseOrDecreaseCriteria(previousLevelValue, levelValue)) {
       continue;
     } else {
-      return false;
+      return [false, levelIndex];
     }
   }
 
-  return true;
+  return [true, -1];
 }
 const input = await Deno.readTextFile("./puzzle02/puzzle02_input.txt");
 
 let answer = 0;
+let part2answer = 0
 for (const line of input.split("\n")) {
-  if (isReportLevelsSafe(line)) {
+
+  const [isSafe, failedAtIndex] = isReportLevelsSafe(line)
+  if (isSafe) {
     answer += 1;
+    part2answer += 1
+    continue
+  } else { 
+    const lineAsArray = line.split(' ')
+    const tolerableLevelCandidate = [...lineAsArray].toSpliced(failedAtIndex, 1).join(' ')
+    const [isSafe] = isReportLevelsSafe(tolerableLevelCandidate)
+    if (isSafe) {
+      part2answer += 1;
+      continue
+    }
+
+
+    if (failedAtIndex > 0) {
+      const tolerableLevelCandidate = [...lineAsArray].toSpliced(failedAtIndex - 1, 1).join(' ')
+      const [isSafe] = isReportLevelsSafe(tolerableLevelCandidate)
+      if (isSafe) {
+        part2answer += 1;
+        continue
+      }
+    }
+
+    if (failedAtIndex < lineAsArray.length - 1) {
+      const tolerableLevelCandidate = [...lineAsArray].toSpliced(failedAtIndex + 1, 1).join(' ')
+      const [isSafe] = isReportLevelsSafe(tolerableLevelCandidate)
+      if (isSafe) {
+        part2answer += 1;
+        continue
+      }
+    }
   }
 }
 console.log("THE ANSWER: ", answer);
+console.log('THE ANSWER PART 2: ', part2answer);
