f269001c4cbd5ef18966999ab2e47ea1e041bd58
Kamil Drzastwa <drzastwakamil@gmail.com>
2024-12-30 11:08:59 +0100

    [AOC] Puzzle 9 - solving part 1

---
+++ puzzle09/puzzle09.ts
@@ -0,0 +1,48 @@
+const input = await Deno.readTextFile("./puzzle09/puzzle09_input.txt");
+
+export function inputToFileBlocks(input: string) {
+  return input.split("").flatMap((value, index) => {
+    const blockLength = Number(value);
+    if (index % 2 === 0) {
+      const representativeIndex = index * 0.5;
+      return Array<string>(blockLength).fill(representativeIndex.toString());
+    }
+    return Array<string>(blockLength).fill(".");
+  }).join("");
+}
+
+export function calculateCheckSumFrom(item: string) {
+  let checkSum = 0;
+  for (let index = 0; index < item.length; index++) {
+    const character = item.at(index);
+    if (character === ".") {
+      continue;
+    }
+
+    const value = Number(character);
+    checkSum += value * index;
+  }
+  return checkSum;
+}
+
+export function shiftFileBlocks(fileBlocks: string) {
+  const blocks = fileBlocks.split("");
+  let leftPointerX = 0;
+  let rightPointerX = fileBlocks.length - 1;
+  while (leftPointerX < rightPointerX) {
+    while (fileBlocks[leftPointerX] !== ".") {
+      leftPointerX += 1;
+    }
+    while (fileBlocks[rightPointerX] === ".") {
+      rightPointerX -= 1;
+    }
+
+    [blocks[leftPointerX], blocks[rightPointerX]] = [
+      blocks[rightPointerX],
+      blocks[leftPointerX],
+    ];
+    leftPointerX += 1;
+    rightPointerX -= 1;
+  }
+  return blocks.join("");
+}
---
+++ puzzle09/puzzle09_test.ts
@@ -0,0 +1,77 @@
+import { assertEquals } from "@std/assert";
+import {
+  calculateCheckSumFrom,
+  inputToFileBlocks,
+  shiftFileBlocks,
+} from "./puzzle09.ts";
+
+Deno.test("calculateCheckSumFrom - test case 1", () => {
+  const result = calculateCheckSumFrom(
+    "01.3.5......",
+  );
+  const expected = 35;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
+
+Deno.test("calculateCheckSumFrom - test case 2", () => {
+  const result = calculateCheckSumFrom(
+    "0099811188827773336446555566..............",
+  );
+  const expected = 1928;
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
+
+Deno.test("inputToFileBlocks - test case 1", () => {
+  const result = inputToFileBlocks(
+    "12345",
+  );
+  const expected = "0..111....22222";
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
+Deno.test("inputToFileBlocks - test case 2", () => {
+  const result = inputToFileBlocks(
+    "2333133121414131402",
+  );
+  const expected = "00...111...2...333.44.5555.6666.777.888899";
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
+
+Deno.test("shiftFileBlocks - test case 1", () => {
+  const result = shiftFileBlocks(
+    "0..111....22222",
+  );
+  const expected = "022111222......";
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
+
+Deno.test("shiftFileBlocks - test case 1", () => {
+  const result = shiftFileBlocks(
+    "00...111...2...333.44.5555.6666.777.888899",
+  );
+  const expected = "0099811188827773336446555566..............";
+  assertEquals(
+    result,
+    expected,
+    `Expected ${expected} but got ${result}`,
+  );
+});
